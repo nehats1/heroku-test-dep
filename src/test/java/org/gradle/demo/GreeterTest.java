6d22b3a8bb9346539174705c20049df8b1f549bc
Nehat Sinani <nehats@metropolia.fi>
2018-04-17 11:01:44 +0300

    first test

---
+++ src/test/java/org/gradle/demo/GreeterTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.gradle.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nehat
+ */
+public class GreeterTest {
+
+    @Test
+    public void dummyUnitTest(){
+        assertEquals(0,1);
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
