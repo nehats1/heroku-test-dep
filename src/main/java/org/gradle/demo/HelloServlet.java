cb625026bd0e5f4c0c7ae2efc39751d975a19890
Nehat Sinani <nehats@metropolia.fi>
2018-04-17 09:30:31 +0300

    First commit

---
+++ src/main/java/org/gradle/demo/HelloServlet.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.gradle.demo;
+
+/**
+ *
+ * @author Nehat
+ */
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet", urlPatterns = {"hello"}, loadOnStartup = 1) 
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.getWriter().print("Hello, World!");  
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name == null) name = "World";
+        request.setAttribute("user", name);
+        request.getRequestDispatcher("response.jsp").forward(request, response); 
+    }
+}
---
+++ src/main/webapp/response.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : response
+    Created on : 10.4.2018, 10:05:15
+    Author     : Nehat
+--%>
+
+
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head>
+        <title>Hello Page</title>
+    </head>
+    <body>
+        <h2>Hello, ${user}!</h2>
+    </body>
+</html>
+
+
